f0efa10435a7c231c05b5c6aaea637bc91d64d7f
Brandon R Barnett <BBarnett@rightpoint.com>
2018-08-29 07:04:47 -0500

    Moving controller to root and defining

---
+++ payment-api/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace payment_api
+{
+    [Route("/")]
+    [ApiController]
+    public class Controller : ControllerBase
+    {
+        [HttpGet("{paymentId}")]
+        public ActionResult<string> GetPaymentStatus()
+        {
+            return "";
+        }
+
+        [HttpPost("submitSync")]
+        public void CreatePaymentSync([FromBody] string payment)
+        {
+        }
+
+        [HttpPost("submitAsync")]
+        public void CreatePaymentAsync([FromBody] string payment)
+        {
+        }
+    }
+}
