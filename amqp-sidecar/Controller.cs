694f8c886c605e41dbcbd94b7aefee50827b8037
Brandon R Barnett <BBarnett@rightpoint.com>
2018-08-29 13:07:36 -0500

    WIP sidecar

--- amqp-sidecar/Controller.cs
+++ amqp-sidecar/Controller.cs
@@ -10,5 +10,30 @@ namespace amqp_sidecar.Controllers
     [ApiController]
     public class Controller : ControllerBase
     {
+        private readonly IConnection _brokerConnection;
+        private readonly HttpClient _httpClient;
+
+        public Controller(IConnection brokerConnection, HttpClient httpClient)
+        {
+            this._brokerConnection = brokerConnection;
+            this._httpClient = httpClient;
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult<string>> CreatePaymentSync([FromBody] SubmitPaymentRequest paymentRequest)
+        {
+            // enqueue request as message
+            using (var channel = this._brokerConnection.CreateModel())
+            {
+                string paymentsExchange = "payments";
+                channel.ExchangeDeclare(exchange: paymentsExchange, type: "topic");
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(paymentProcessorRequest));
+                channel.BasicPublish(exchange: paymentsExchange,
+                                     routingKey: "payments.create",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
     }
 }
--- amqp-sidecar/Startup.cs
+++ amqp-sidecar/Startup.cs
@@ -26,6 +26,18 @@ namespace amqp_sidecar
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // RabbitMQ connection. Connections are thread safe, models are not
+            var factory = new ConnectionFactory
+            {
+                HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME"),
+                UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
+                Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD")
+            };
+            services.AddSingleton(factory.CreateConnection());
+
+            // HttpClient
+            services.AddSingleton(new HttpClient());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
